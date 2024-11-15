0afbf62e4726b8748a04dfa3c3389d53bbd2f0e5
Farit Espinoza <faritespinoza@gmail.com>
2024-11-14 21:04:37 -0500

    Inicios en Python

---
+++ dia_1/1_variables.py
@@ -0,0 +1,23 @@
+# Tipos de variables primitivos
+# String
+nombre = 'Eduardo'
+direccion = "Las Lechuzas '512"
+# Cuando queremos guardar un texto muy largo que además puede tener saltos de línea
+enunciado = '''Hola
+como estas
+        Te extraño
+Espero verte pronto
+    Saludos
+        Eduardo'''
+otro_enunciado = """Hola
+como estas
+        Te extraño
+Espero verte pronto
+    Saludos
+        Eduardo"""
+# Int
+edad = 32
+# Boolean
+casado = True
+# Float
+estatura = 1.82

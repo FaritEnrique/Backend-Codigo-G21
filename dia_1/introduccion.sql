1fd436756ba8b6ac66cada92386bc257733a95d0
Farit Espinoza <faritespinoza@gmail.com>
2024-11-20 21:15:59 -0500

    inicios postgres

--- dia_1/introduccion.sql
+++ dia_1/introduccion.sql
@@ -1,3 +1,6 @@
--- Así se define un comentario en sql
-
+-- Asi se define un comentario en las bases de datos
+-- DDL (Data Definition Language) es un sublenguaje de SQL que sirve para definir como se almacenaran los datos
 CREATE DATABASE pruebas;
+
+-- Limpiara la terminal de nuestro psql
+\! cls
